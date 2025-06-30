6a959016a77220f1f92274d41b03f28b85b928c0
Mohit Saini <145444460+gitupmohit@users.noreply.github.com>
2025-06-30 18:48:06 +0530

    Time: 11 ms (37.93%) | Memory: 56.6 MB (70.34%) - LeetSync

--- 148-sort-list/sort-list.java
+++ 148-sort-list/sort-list.java
@@ -8,29 +8,77 @@
  *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
  * }
  */
+// class Solution {
+//     public ListNode sortList(ListNode head) {
+//         ListNode temp = head;
+//         ArrayList<Integer> list = new ArrayList<>();
+//         while(temp != null){
+//             list.add(temp.val);
+//             temp = temp.next;
+//         }
+//         int n = list.size();
+//         int[] arr = new int[n];
+//         for(int i=0; i<n; i++){
+//             arr[i] = list.get(i);
+//         }
+//         Arrays.sort(arr);
+
+//         int i =0;
+//         ListNode output = head;
+//         while(output != null){
+//             output.val = arr[i];
+//             output = output.next;
+//             i++;
+//         }
+//         return head;
+
+//     }
+// }
+
 class Solution {
     public ListNode sortList(ListNode head) {
-        ListNode temp = head;
-        ArrayList<Integer> list = new ArrayList<>();
-        while(temp != null){
-            list.add(temp.val);
-            temp = temp.next;
-        }
-        int n = list.size();
-        int[] arr = new int[n];
-        for(int i=0; i<n; i++){
-            arr[i] = list.get(i);
-        }
-        Arrays.sort(arr);
+        if (head == null || head.next == null) return head;
 
-        int i =0;
-        ListNode output = head;
-        while(output != null){
-            output.val = arr[i];
-            output = output.next;
-            i++;
-        }
-        return head;
+        // Step 1: Split the list into halves
+        ListNode mid = getMiddle(head);
+        ListNode right = mid.next;
+        mid.next = null;
 
+        // Step 2: Sort both halves
+        ListNode leftSorted = sortList(head);
+        ListNode rightSorted = sortList(right);
+
+        // Step 3: Merge both sorted halves
+        return merge(leftSorted, rightSorted);
+    }
+
+    private ListNode getMiddle(ListNode head) {
+        ListNode slow = head, fast = head;
+
+        while (fast.next != null && fast.next.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+
+        return slow;
+    }
+
+    private ListNode merge(ListNode l1, ListNode l2) {
+        ListNode dummy = new ListNode(0), tail = dummy;
+
+        while (l1 != null && l2 != null) {
+            if (l1.val < l2.val) {
+                tail.next = l1;
+                l1 = l1.next;
+            } else {
+                tail.next = l2;
+                l2 = l2.next;
+            }
+            tail = tail.next;
+        }
+
+        tail.next = (l1 != null) ? l1 : l2;
+
+        return dummy.next;
     }
 }
