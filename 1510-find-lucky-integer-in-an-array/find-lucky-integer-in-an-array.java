bbbcd03e538a37de2f7b4b54cc705146993d8c0b
Mohit Saini <145444460+gitupmohit@users.noreply.github.com>
2025-07-05 10:11:50 +0530

    Time: 1 ms (99.94%) | Memory: 43 MB (93.60%) - LeetSync

--- 1510-find-lucky-integer-in-an-array/find-lucky-integer-in-an-array.java
+++ 1510-find-lucky-integer-in-an-array/find-lucky-integer-in-an-array.java
@@ -1,18 +1,34 @@
 class Solution {
     public int findLucky(int[] arr) {
-        Arrays.sort(arr);
-        int n = arr.length;
-        HashMap<Integer, Integer> map = new HashMap<>();
+        // Arrays.sort(arr);
+        // int n = arr.length;
+        // HashMap<Integer, Integer> map = new HashMap<>();
 
-        for (int i = 0; i < n; i++) {
-            map.put(arr[i], map.getOrDefault(arr[i], 0) + 1);
+        // for (int i = 0; i < n; i++) {
+        //     map.put(arr[i], map.getOrDefault(arr[i], 0) + 1);
+        // }
+
+        // for (int i = n-1; i >= 0; i--) {
+        //     if (map.get(arr[i]) == arr[i]) {
+        //         return arr[i];
+        //     }
+        // }
+        // return -1;
+
+         int[] freq = new int[501]; // Since 1 <= arr[i] <= 500
+
+        // Count frequency of each number
+        for (int num : arr) {
+            freq[num]++;
         }
 
-        for (int i = n-1; i >= 0; i--) {
-            if (map.get(arr[i]) == arr[i]) {
-                return arr[i];
+        // Check from largest to smallest
+        for (int i = 500; i >= 1; i--) {
+            if (freq[i] == i) {
+                return i;
             }
         }
+
         return -1;
     }
 }
