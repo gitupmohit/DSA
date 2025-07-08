5bfbdd71f4b78e5681907497574fcb111c0fd875
Mohit Saini <145444460+gitupmohit@users.noreply.github.com>
2025-07-09 03:54:13 +0530

    Time: 3 ms (97.28%) | Memory: 57.9 MB (65.56%) - LeetSync

--- 1984-maximum-distance-between-a-pair-of-values/maximum-distance-between-a-pair-of-values.java
+++ 1984-maximum-distance-between-a-pair-of-values/maximum-distance-between-a-pair-of-values.java
@@ -1,17 +1,39 @@
 class Solution {
     public int maxDistance(int[] nums1, int[] nums2) {
-        int i = 0, j = 0, max = 0;
+        // ArrayList<ArrayList<Integer>> list = new ArrayList<>();
+        int n = nums1.length;
+        int m = nums2.length;
 
-        while (i < nums1.length && j < nums2.length) {
+        // for (int i = 0; i < n; i++) {
+        //     for (int j = i; j < m; j++) {
+        //         ArrayList<Integer> current = new ArrayList<>();
+        //         if (nums1[i] <= nums2[j]) {
+        //             current.add(i);
+        //             current.add(j);
+        //             list.add(current);
+        //         }
+        //     }
+        // }
+        // int max = 0;
+        // for(int i=0; i<list.size(); i++){
+        //     int first = list.get(i).get(0);
+        //     int sec = list.get(i).get(1);
+        //     max = Math.max(max, sec-first);
+        // }
+        // return max;
+        int max = 0;
+        int i =0;
+        int j =0;
+
+        while(i<n && j<m){
             if (nums1[i] <= nums2[j]) {
-                max = Math.max(max, j - i);
+                max = Math.max(max, j-i);
                 j++;
-            } else {
+            }else{
                 i++;
-                if (i > j) j = i;
+                if(i > j) j =i;
             }
         }
-
         return max;
     }
 }
