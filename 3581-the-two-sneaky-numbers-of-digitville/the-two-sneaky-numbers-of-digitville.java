a13181f64412c12faa3eb8aae7c8f0a6cfb131cf
Mohit Saini <145444460+gitupmohit@users.noreply.github.com>
2025-07-11 16:07:00 +0530

    Time: 1 ms (100.00%) | Memory: 44.6 MB (76.91%) - LeetSync

--- 3581-the-two-sneaky-numbers-of-digitville/the-two-sneaky-numbers-of-digitville.java
+++ 3581-the-two-sneaky-numbers-of-digitville/the-two-sneaky-numbers-of-digitville.java
@@ -1,8 +1,10 @@
 class Solution {
     public int[] getSneakyNumbers(int[] nums) {
         int n = nums.length;
-        int[] arr = new int[2];
-        int index =0;
+        int k =-1;
+        int l =-1;
+        // int[] arr = new int[2];
+        // int index =0;
         // HashSet<Integer> set = new HashSet<>();
 
         // for(int i=0; i<n; i++){
@@ -15,14 +17,27 @@ class Solution {
         // }
         // return arr;
 
-        Arrays.sort(nums);
+        // Arrays.sort(nums);
 
-        for(int i=1; i<n; i++){
-            if(nums[i] == nums[i-1]){
-                arr[index] = nums[i];
-                index++;
+        // for(int i=1; i<n; i++){
+        //     if(nums[i] == nums[i-1]){
+        //         arr[index] = nums[i];
+        //         index++;
+        //     }
+        // }
+        // return arr;
+
+        int[] hash = new int[n];
+        for(int i=0; i<n; i++){
+            if(hash[nums[i]] == 1){
+                if(k == -1){
+                    k=nums[i];
+                }else{
+                    l =nums[i];
+                }
             }
+            hash[nums[i]]++;
         }
-        return arr;
+        return new int[] {k,l};
     }
 }
