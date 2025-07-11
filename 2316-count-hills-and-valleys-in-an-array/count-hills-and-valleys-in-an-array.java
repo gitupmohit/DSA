743d849673451d9589b351e6131cf4691e1acd28
Mohit Saini <145444460+gitupmohit@users.noreply.github.com>
2025-07-12 04:30:13 +0530

    Time: 0 ms (100.00%) | Memory: 42 MB (22.27%) - LeetSync

--- 2316-count-hills-and-valleys-in-an-array/count-hills-and-valleys-in-an-array.java
+++ 2316-count-hills-and-valleys-in-an-array/count-hills-and-valleys-in-an-array.java
@@ -1,29 +1,38 @@
 class Solution {
     public int countHillValley(int[] nums) {
-        int count =0;
-        int n = nums.length;
+        // int count =0;
+        // int n = nums.length;
 
-        for(int i=1; i<n-1; i++){
-            int left = -1;
-            int right = -1;
-            for(int j= 0; j<n; j++){
-                if(j<i && nums[j] != nums[i]){
-                    left = nums[j];
-                }else if(j>i && nums[j] != nums[i]){
-                    right = nums[j];
-                    break;
-                }
-            }
-            if(nums[i] == nums[i-1]){
-                continue;
-            }else if(left == -1 || right == -1){
-                continue;
-            }else if(nums[i] > left && nums[i] > right){
-                count++;
-            }else if(nums[i] < left && nums[i] <right){
-                count++;
+        // for(int i=1; i<n-1; i++){
+        //     int left = -1;
+        //     int right = -1;
+        //     for(int j= 0; j<n; j++){
+        //         if(j<i && nums[j] != nums[i]){
+        //             left = nums[j];
+        //         }else if(j>i && nums[j] != nums[i]){
+        //             right = nums[j];
+        //             break;
+        //         }
+        //     }
+        //     if(nums[i] == nums[i-1]){
+        //         continue;
+        //     }else if(left == -1 || right == -1){
+        //         continue;
+        //     }else if(nums[i] > left && nums[i] > right){
+        //         count++;
+        //     }else if(nums[i] < left && nums[i] <right){
+        //         count++;
+        //     }
+        // }
+        // return count;
+        int res=0;
+        int fval=nums[0];
+        for(int i=1; i<nums.length-1; i++){
+            if(fval < nums[i] && nums[i] > nums[i+1] || fval > nums[i] && nums[i] < nums[i+1]){
+                fval=nums[i];
+                res++;
             }
         }
-        return count;
+        return res;
     }
 }
