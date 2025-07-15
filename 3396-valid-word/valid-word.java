0d8436fff5be6d633ff3a4187f6556460111aac0
Mohit Saini <145444460+gitupmohit@users.noreply.github.com>
2025-07-15 18:42:55 +0530

    Time: 1 ms (99.12%) | Memory: 42 MB (62.25%) - LeetSync

---
+++ 3396-valid-word/valid-word.java
@@ -0,0 +1,32 @@
+class Solution {
+    public boolean isValid(String word) {
+        int n = word.length();
+        if(n < 3) return false;
+        boolean digit = false;
+        boolean vowel = false;
+        boolean consonant = false;
+        
+        for(int i=0; i<n; i++){
+            char ch = word.charAt(i);
+            if(ch == 'a' || ch == 'e' || ch == 'i' || ch == 'o' || ch == 'u' || ch == 'A' || ch == 'E' || ch == 'I' || ch == 'O' || ch == 'U'){
+                vowel = true;
+            }else if(ch >= '0' && ch <= '9'){
+                digit = true;
+            }else if(ch >= 'a' && ch <='z' || ch >= 'A' && ch <='Z'){
+                consonant = true;
+            }else{
+                return false;
+            }
+        }
+        // if(digit == false && consonant == false){
+        //     return false;
+        // }else if(vowel == false){
+        //     return false;
+        // }else if(consonant == false){
+        //     return false;
+        // }else return true;
+        return vowel && consonant;
+       
+
+    }
+}
