60e877d66d6db44c523260cb5f556a127ccfec12
Mohit Saini <145444460+gitupmohit@users.noreply.github.com>
2025-07-16 13:51:17 +0530

    Time: 16 ms (14.75%) | Memory: 62.3 MB (59.84%) - LeetSync

--- 3490-find-the-maximum-length-of-valid-subsequence-i/find-the-maximum-length-of-valid-subsequence-i.java
+++ 3490-find-the-maximum-length-of-valid-subsequence-i/find-the-maximum-length-of-valid-subsequence-i.java
@@ -8,44 +8,55 @@ class Solution {
         boolean even = true;
         boolean odd = true;
 
-
-        for(int i =0; i<n; i++){
-            if(nums[i] %2 == 0){
+        for (int i = 0; i < n; i++) {
+            if (nums[i] % 2 == 0) {
                 el++;
-            }  
+            }
         }
-        for(int i =0; i<n; i++){
-            if(nums[i] %2 != 0){
+        for (int i = 0; i < n; i++) {
+            if (nums[i] % 2 != 0) {
                 ol++;
             }
         }
-        for(int i =0; i<n; i++){
-            if(even == true){
-                if(nums[i] % 2 == 0){
+        for (int i = 0; i < n; i++) {
+            if (even == true) {
+                if (nums[i] % 2 == 0) {
                     eol++;
                     even = !even;
                 }
-            }else if(even == false){
-                if(nums[i] % 2 != 0){
+            } else if (even == false) {
+                if (nums[i] % 2 != 0) {
                     eol++;
                     even = !even;
                 }
             }
         }
-        for(int i =0; i<n; i++){
-            if(odd == true){
-                if(nums[i] % 2 != 0){
+        for (int i = 0; i < n; i++) {
+            if (odd == true) {
+                if (nums[i] % 2 != 0) {
                     oel++;
                     odd = !odd;
                 }
-            }else if(odd == false){
-                if(nums[i] % 2 == 0){
+            } else if (odd == false) {
+                if (nums[i] % 2 == 0) {
                     oel++;
                     odd = !odd;
                 }
             }
         }
-        return Math.max(el,Math.max(ol,Math.max(eol,oel)));
-        
+        int max = el;
+
+        if (ol > max) {
+            max = ol;
+        }
+        if (eol > max) {
+            max = eol;
+        }
+        if (oel > max) {
+            max = oel;
+        }
+
+        return max;
+
     }
 }
