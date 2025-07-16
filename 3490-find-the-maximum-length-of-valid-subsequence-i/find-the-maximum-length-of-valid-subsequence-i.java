4849e91401bf868da8e57416632626f055670470
Mohit Saini <145444460+gitupmohit@users.noreply.github.com>
2025-07-16 13:48:52 +0530

    Time: 16 ms (14.75%) | Memory: 62 MB (84.43%) - LeetSync

---
+++ 3490-find-the-maximum-length-of-valid-subsequence-i/find-the-maximum-length-of-valid-subsequence-i.java
@@ -0,0 +1,51 @@
+class Solution {
+    public int maximumLength(int[] nums) {
+        int n = nums.length;
+        int el = 0;
+        int ol = 0;
+        int eol = 0;
+        int oel = 0;
+        boolean even = true;
+        boolean odd = true;
+
+
+        for(int i =0; i<n; i++){
+            if(nums[i] %2 == 0){
+                el++;
+            }  
+        }
+        for(int i =0; i<n; i++){
+            if(nums[i] %2 != 0){
+                ol++;
+            }
+        }
+        for(int i =0; i<n; i++){
+            if(even == true){
+                if(nums[i] % 2 == 0){
+                    eol++;
+                    even = !even;
+                }
+            }else if(even == false){
+                if(nums[i] % 2 != 0){
+                    eol++;
+                    even = !even;
+                }
+            }
+        }
+        for(int i =0; i<n; i++){
+            if(odd == true){
+                if(nums[i] % 2 != 0){
+                    oel++;
+                    odd = !odd;
+                }
+            }else if(odd == false){
+                if(nums[i] % 2 == 0){
+                    oel++;
+                    odd = !odd;
+                }
+            }
+        }
+        return Math.max(el,Math.max(ol,Math.max(eol,oel)));
+        
+    }
+}
