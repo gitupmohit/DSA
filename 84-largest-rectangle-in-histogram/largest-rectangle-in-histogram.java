b4a31283077827255a61a035601bd1f165a30b59
Mohit Saini <145444460+gitupmohit@users.noreply.github.com>
2025-08-01 16:07:32 +0530

    Time: 68 ms (66.18%) | Memory: 57.2 MB (80.01%) - LeetSync

--- 84-largest-rectangle-in-histogram/largest-rectangle-in-histogram.java
+++ 84-largest-rectangle-in-histogram/largest-rectangle-in-histogram.java
@@ -3,21 +3,17 @@ class Solution {
         int n = heights.length;
         Stack<Integer> st = new Stack<>();
         int maxArea = 0;
-        int i = 0;
 
-        while (i <= n) {
+        for (int i = 0; i <= n; i++) {
+            // Use 0 as sentinel height at the end
             int h = (i == n) ? 0 : heights[i];
-
-            if (st.isEmpty() || h >= heights[st.peek()]) {
-                st.push(i);
-                i++;
-            } else {
+            
+            while (!st.isEmpty() && h < heights[st.peek()]) {
                 int height = heights[st.pop()];
                 int width = st.isEmpty() ? i : i - st.peek() - 1;
-                int area = height * width;
-                maxArea = Math.max(maxArea, area);
-                // Note: We do not increment i here to reprocess current bar after pop
+                maxArea = Math.max(maxArea, height * width);
             }
+            st.push(i);
         }
 
         return maxArea;
