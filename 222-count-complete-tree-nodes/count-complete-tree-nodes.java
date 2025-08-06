14a29ab8323ef3fe170110db02f447646c709b62
Mohit Saini <145444460+gitupmohit@users.noreply.github.com>
2025-08-06 17:11:15 +0530

    Time: 0 ms (100.00%) | Memory: 48.2 MB (5.45%) - LeetSync

--- 222-count-complete-tree-nodes/count-complete-tree-nodes.java
+++ 222-count-complete-tree-nodes/count-complete-tree-nodes.java
@@ -15,16 +15,34 @@
  */
 class Solution {
     public int countNodes(TreeNode root) {
-    List<Integer> result = new ArrayList<>();
-    helper(root, result);
-    return result.size(); // number of nodes
-}
+        if (root == null) return 0;
 
-private void helper(TreeNode root, List<Integer> result) {
-    if (root == null) return;
-    result.add(root.val);
-    helper(root.left, result);
-    helper(root.right, result);
-}
+        int lh = getLeftHeight(root);
+        int rh = getRightHeight(root);
 
+        if (lh == rh) {
+            // tree is perfect binary tree => nodes = 2^height - 1
+            return (int)Math.pow(2, lh) - 1;
+        }
+
+        return 1 + countNodes(root.left) + countNodes(root.right);
+    }
+
+    private int getLeftHeight(TreeNode node) {
+        int height = 0;
+        while (node != null) {
+            height++;
+            node = node.left;
+        }
+        return height;
+    }
+
+    private int getRightHeight(TreeNode node) {
+        int height = 0;
+        while (node != null) {
+            height++;
+            node = node.right;
+        }
+        return height;
+    }
 }
