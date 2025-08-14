83112d7ba56d258a270bfeea644f4d42aa15a603
Mohit Saini <145444460+gitupmohit@users.noreply.github.com>
2025-08-14 18:08:18 +0530

    Time: 0 ms (100.00%) | Memory: 45.5 MB (6.48%) - LeetSync

--- 26-remove-duplicates-from-sorted-array/remove-duplicates-from-sorted-array.java
+++ 26-remove-duplicates-from-sorted-array/remove-duplicates-from-sorted-array.java
@@ -10,27 +10,27 @@ class Solution {
         // // }
         // // return i;
 
-        // int n = nums.length;
-        // int j=1;
+        int n = nums.length;
+        int j=1;
 
-        // for(int i = 1;i<n;i++){
-        //     if(nums[i]!=nums[i-1]){
-        //         nums[j]=nums[i];
-        //         j++;
-        //     }
-        // }
-        // return j;
-       
-        int n= nums.length;
-        int j =0;
-        int prev = -102;
-        for(int i=0; i<n; i++){
-            if(nums[i] != prev){
-                nums[j] = nums[i];
+        for(int i = 1;i<n;i++){
+            if(nums[i]!=nums[i-1]){
+                nums[j]=nums[i];
                 j++;
-                prev = nums[i];
             }
         }
         return j;
+       
+        // int n= nums.length;
+        // int j =0;
+        // int prev = -102;
+        // for(int i=0; i<n; i++){
+        //     if(nums[i] != prev){
+        //         nums[j] = nums[i];
+        //         j++;
+        //         prev = nums[i];
+        //     }
+        // }
+        // return j;
     }
 }
