ccfc32149f3068b187aa628319caf200af9c73e9
Mohit Saini <145444460+gitupmohit@users.noreply.github.com>
2025-09-30 12:34:38 +0530

    Time: 42 ms (86.32%) | Memory: 44.7 MB (62.42%) - LeetSync

--- 2324-find-triangular-sum-of-an-array/find-triangular-sum-of-an-array.java
+++ 2324-find-triangular-sum-of-an-array/find-triangular-sum-of-an-array.java
@@ -1,33 +1,11 @@
 class Solution {
     public int triangularSum(int[] nums) {
         int n = nums.length;
-        Queue<Integer> q = new LinkedList<>();
-        
-        // Push all elements into queue
-        for(int it : nums){
-            q.offer(it);
-        }
-
-        // Perform n-1 reductions
-        for(int i = 0; i < n - 1; i++){
-            ArrayList<Integer> list = new ArrayList<>();
-            int k = q.size();
-            
-            // Generate next level by summing adjacent elements
-            for(int l = 0; l < k - 1; l++){
-                int val = q.poll();
-                int sum = val + q.peek();
-                list.add(sum % 10);
-            }
-
-            // Clear the queue and insert new level
-            q.clear();
-            for(int num : list){
-                q.offer(num);
+        for(int i = n - 1; i > 0; i--){
+            for(int j = 0; j < i; j++){
+                nums[j] = (nums[j] + nums[j + 1]) % 10;
             }
         }
-
-        // Final result will be the only element left
-        return q.peek();
+        return nums[0];
     }
 }
